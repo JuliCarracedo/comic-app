0dab62999a3f639163d36cd4e5c4c0d48dfc75d2
JuliCarracedo <julicarra@hotmail.com>
2022-02-22 18:57:20 -0300

    add JWT, sesssions controller and session and registration responses

--- app/models/v1/user.rb
+++ app/models/v1/user.rb
@@ -1,11 +1,18 @@
-class User < ApplicationRecord
+class V1::User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :username, presence: true, uniqueness: true, length: {max: 20}
+  validates :username, presence: true, uniqueness: true, length: {max: 20}, allow_blank: false, format: { with: /\A[a-zA-Z0-9]+\z/ }
   validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i}
   validates :password, presence: true, length: {in: 8...20}, format: {with: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/}
+
+
+  def generate_jwt
+    JWT.encode({ id: id,
+                exp: 60.days.from_now.to_i },
+               Rails.application.secrets.secret_key_base)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  scope :api, defaults: { format: :json } do
+    devise_for :users, controllers: { sessions: :sessions },
+                       path_names: { sign_in: :login }
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

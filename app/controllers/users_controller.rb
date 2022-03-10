573df2a0d661693d2a69a894f210f732872407c8
JuliCarracedo <julicarra@hotmail.com>
2022-03-10 13:39:49 -0300

    Add image attachment structure to User model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,28 @@
 class UsersController < ApplicationController
     before_action :authenticate_user!
-
+    protect_from_forgery with: :null_session
     def show
         
     end
 
-    def update
-      if current_user.update_attributes(user_params)
-        render :show
+    # def update
+    #   if current_user.update_attributes(user_params)
+    #     render :show
+    #   else
+    #     render json: { errors: current_user.errors }, status: :unprocessable_entity
+    #   end
+    # end
+
+    def upload_profile
+      if current_user.image
+        current_user.image.purge
+        current_user.image.build(:image => params[:image])
+        render json: {message: 'Profile Picture updated'}, status: :ok
       else
-        render json: { errors: current_user.errors }, status: :unprocessable_entity
+        current_user.image.build(:image => params[:image])
+        render json: {message: 'Profile Picture uploaded'}, status: :ok
       end
     end
 
     private
-
-    def user_params
-      params.require(:user).permit(:username, :email, :password)
-    end
 end

0d4c4ad863c0f9ce62a439afe402d7e33e90bd29
JuliCarracedo <julicarra@hotmail.com>
2022-03-10 13:09:38 -0300

    Update prtect from forgery parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery unless: -> { request.format.json? }
+    protect_from_forgery with: :null_session
     
     respond_to :json
     

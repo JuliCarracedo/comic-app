f402e49ef3353d30c96ad90189df45d95e3738a9
JuliCarracedo <julicarra@hotmail.com>
2022-03-16 20:47:51 -0300

    Fix variable wrong name for Comic

--- app/models/comic.rb
+++ app/models/comic.rb
@@ -1,7 +1,7 @@
 class Comic < ApplicationRecord
   belongs_to :user
 
-  validates :name, presence: true, uniqueness: true, length: {in: 1...30}
+  validates :title, presence: true, uniqueness: true, length: {in: 1...30}
   validates :description, presence: true, length: {in: 20...200}
 
 end

ceaaa50adae73bb64e553c3240c32f1af11412f0
JuliCarracedo <julicarra@hotmail.com>
2022-03-16 21:06:42 -0300

    Add user update and destroy methods in controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,8 +19,22 @@ class UsersController < ApplicationController
     #     render json: { errors: current_user.errors }, status: :unprocessable_entity
     #   end
     # end
+    def update
+      if current_user.update(user_params)
+        render json: {message: "Successfully updated"}, status: 200
+      else
+        render json: {error: current_user.errors}, status: 422
+      end
+    end
 
-
+    def delete
+      current_user.destroy
+      current_user = nil;
+      render json: {messsage: "Successfully deleted"}, status: 200
+    end
 
     private
+    def user_params
+      params.require(:user).permit(:username)
+    end
 end

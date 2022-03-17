f505769853dd1db2367e6321a2abace318cdca3f
JuliCarracedo <julicarra@hotmail.com>
2022-03-16 21:07:21 -0300

    Update Routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   scope :api, defaults: { format: :json } do
     devise_for :users, controllers: { sessions: :sessions, registrations: :registrations },
                        path_names: { sign_in: :login }
-    resource :user, only: [:show, :update]
+    resource :user, only: [:show, :update, :delete]
     post 'user/upload', to: 'users#upload_profile'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

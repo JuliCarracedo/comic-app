d11d29c41bb426575e771bff07e66892a81b0191
JuliCarracedo <julicarra@hotmail.com>
2022-03-22 13:13:58 -0300

    Create FollowersController

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
                        path_names: { sign_in: :login }
     resource :users, only: [:show, :update, :delete]
     resource :comics, only: [:show, :create, :update, :delete]
-    post 'user/upload', to: 'users#upload_profile'
+    get "/followed", to "follower#list"
+    # post 'user/upload', to: 'users#upload_profile'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

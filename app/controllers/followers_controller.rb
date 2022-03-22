56994934a12055e6e3e6c8b77f9b1d7ed02fd0d9
JuliCarracedo <julicarra@hotmail.com>
2022-03-22 13:18:07 -0300

    Add followed_comics endpoint

--- app/controllers/followers_controller.rb
+++ app/controllers/followers_controller.rb
@@ -1,7 +1,7 @@
 class FollowersController < ApplicationController
     before_action :authenticate_user!
     def list
-        followed = Follower.where(user_id: current_user_id).map(follower => follower.comic)
+        followed = Follower.where(user_id: current_user.id).map{|follower| follower.comic}
         render json: {followed_comics: followed}, status:200
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
                        path_names: { sign_in: :login }
     resource :users, only: [:show, :update, :delete]
     resource :comics, only: [:show, :create, :update, :delete]
-    get "/followed", to "follower#list"
+    get "/followed_comics", to: "followers#list"
     # post 'user/upload', to: 'users#upload_profile'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

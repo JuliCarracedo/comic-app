45ac8ac915dc23b7326fbccab4d29fe759c2c3c9
JuliCarracedo <julicarra@hotmail.com>
2022-03-24 13:35:00 -0300

    Add follow endpoint in Follower Controller

--- app/controllers/followers_controller.rb
+++ app/controllers/followers_controller.rb
@@ -4,4 +4,14 @@ class FollowersController < ApplicationController
         followed = Follower.where(user_id: current_user.id).map{|follower| follower.comic}
         render json: {followed_comics: followed}, status:200
     end
+    def follow
+        if Follower.find_by(user_id: current_user.id, comic_id: params[:comic_id])
+            render json: {error: "Already followed"}, status:422
+        else
+            follower.new(user_id: current_user.id, comic_id: params[:comic_id])
+            if follower.save
+                render json: {message: "Comic Followed Successfully"}, status:200
+            end
+        end
+    end
 end

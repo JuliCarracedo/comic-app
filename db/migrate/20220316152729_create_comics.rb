c2f620a1191b6ce0d7708d83838b6a8ed68bed5f
JuliCarracedo <julicarra@hotmail.com>
2022-03-24 19:21:27 -0300

    Add comic URL to migration

--- db/migrate/20220316152729_create_comics.rb
+++ db/migrate/20220316152729_create_comics.rb
@@ -3,6 +3,7 @@ class CreateComics < ActiveRecord::Migration[6.1]
     create_table :comics do |t|
       t.string :title
       t.text :description
+      t.string :thumbnail_url
       t.references :user, null: false, foreign_key: true
 
       t.timestamps

d38d5adf71b632993b763740fc7edb7f27c197f6
JuliCarracedo <julicarra@hotmail.com>
2022-03-30 15:40:20 -0300

    Refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,14 @@ Rails.application.routes.draw do
   scope :api, defaults: { format: :json } do
     devise_for :users, controllers: { sessions: :sessions, registrations: :registrations },
                        path_names: { sign_in: :login }
-    resource :users, only: [:show, :update, :delete]
-    resource :comics, only: [:show, :create, :update, :delete]
-    resource :likes, only: [:create, :delete]
-    get "/followed_comics", to: "followers#list"
-    post "/followed_comics", to: "followers#follow"
-    delete "/followed_comics", to: "followers#unfollow"
+    resources :users, only: [:show, :update, :delete]
+    resources :comics, only: [:show, :create, :update, :delete] do
+      get "/followers", to: "followers#list"
+      post "/follow", to: "followers#follow"
+      delete "/unfollow", to: "followers#unfollow"
+    end
+    resources :likes, only: [:create, :delete]
+    
     # post 'user/upload', to: 'users#upload_profile'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

305b0b0d09a4d5fef2887612f78e1d75748e6adb
JuliCarracedo <julicarra@hotmail.com>
2022-03-30 15:35:11 -0300

    Check that a user can't follow their own comic

--- app/models/follower.rb
+++ app/models/follower.rb
@@ -1,4 +1,15 @@
 class Follower < ApplicationRecord
   belongs_to :user, inverse_of: :following
   belongs_to :comic, inverse_of: :followers
+
+  validate :is_not_owner
+
+  private
+
+  def is_not_owner
+    if current_user.id === user_id
+      errors.add(:user, "can't follow your own commic")
+    end
+  end
+
 end

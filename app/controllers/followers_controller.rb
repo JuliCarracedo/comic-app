96b3139d6fad7b5df819e677a0a1925505f0d739
JuliCarracedo <julicarra@hotmail.com>
2022-03-30 15:53:07 -0300

    List now shows the followers to a comic

--- app/controllers/followers_controller.rb
+++ app/controllers/followers_controller.rb
@@ -2,8 +2,8 @@ class FollowersController < ApplicationController
     before_action :authenticate_user!
 
     def list
-        followed = Follower.where(user_id: current_user.id).map{|follower| follower.comic}
-        render json: {followed_comics: followed}, status:200
+        followers = Follower.where(comic_id: params[:comic_id]).map{|follower| follower.comic}
+        render json: {followers: followers}, status:200
     end
 
     def follow

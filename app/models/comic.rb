a12b856fab50cf191ee91ff90d2bc45088f05d9f
JuliCarracedo <julicarra@hotmail.com>
2022-04-05 12:30:12 -0300

    Add dependent:destroy

--- app/models/comic.rb
+++ app/models/comic.rb
@@ -1,7 +1,7 @@
 class Comic < ApplicationRecord
   belongs_to :user
-  has_many :followers
-  has_many :likes
+  has_many :followers, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
   validates :title, presence: true, uniqueness: true, length: {in: 1...30}
   validates :description, presence: true, length: {in: 20...200}

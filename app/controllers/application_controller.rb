7ee02bb46ccbf3fe7dbbaa67c6e270783b4fc440
JuliCarracedo <julicarra@hotmail.com>
2022-04-06 21:12:10 -0300

    Fix syntax

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,12 +15,13 @@ class ApplicationController < ActionController::Base
     def authenticate_user
         if request.headers['Authorization'].present?
             authenticate_or_request_with_http_token do |token|
-            begin
-                jwt_payload = JWT.decode(token, Rails.application.secrets.secret_key_base).first
-                @current_user_id = jwt_payload['id']
+                begin
+                    jwt_payload = JWT.decode(token, Rails.application.secrets.secret_key_base).first
+                    @current_user_id = jwt_payload['id']
 
-            rescue JWT::ExpiredSignature, JWT::VerificationError, JWT::DecodeError
-                head :unauthorized
+                rescue JWT::ExpiredSignature, JWT::VerificationError, JWT::DecodeError
+                    head :unauthorized
+                end
             end
         end
     end

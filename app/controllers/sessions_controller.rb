42483b30e8304fdc691f60edd04472a66abc7585
JuliCarracedo <julicarra@hotmail.com>
2022-04-12 17:09:23 -0300

    Refactor password verification

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,15 @@
 class SessionsController < Devise::SessionsController
-  skip_before_action :authorize_request
+  skip_before_action :authorize_request, :verify_authenticity_token
 
     def create
       @user = User.find_by_email(params[:email])
-      if @user&.authenticate(params[:password])
+      if @user.valid_password?(params[:password])
         token = JsonWebToken.encode(user_id: @user.id)
         time = Time.now + 24.hours.to_i
         render json: { token: token, exp: time.strftime("%m-%d-%Y %H:%M"),
                       username: @user.username }, status: :ok
       else
-        render json: { error: 'unauthorized' }, status: :unauthorized
+        render json: { error: 'unauthorized'}, status: :unauthorized
       end
     end
 end

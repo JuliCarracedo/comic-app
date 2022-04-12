4e784ca621828900fc1506c9b3f4c5fed244678d
JuliCarracedo <julicarra@hotmail.com>
2022-04-12 16:52:47 -0300

    Update SessionsController

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,16 @@
 class SessionsController < Devise::SessionsController
-  skip_before_action :authenticate_user
+  skip_before_action :authorize_request
 
     def create
-      puts ('console check')
-      puts(params)
-      user = User.find_by_email(params[:email])
-      if user && user.valid_password?(params[:password])
-        @current_user = user
-        render json: { message: 'Logged in', token: user.generate_jwt }, status: :ok
+      @user = User.find_by_email(params[:email])
+      if @user&.authenticate(params[:password])
+        token = JsonWebToken.encode(user_id: @user.id)
+        time = Time.now + 24.hours.to_i
+        render json: { token: token, exp: time.strftime("%m-%d-%Y %H:%M"),
+                      username: @user.username }, status: :ok
       else
-        render json: { errors: { 'email or password' => ['is invalid'] } }, status: :unprocessable_entity
+        render json: { error: 'unauthorized' }, status: :unauthorized
       end
     end
 end
+

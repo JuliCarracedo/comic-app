8ba657dfa6f7ea2673d748196fa7c04202cc5dcc
JuliCarracedo <julicarra@hotmail.com>
2022-04-12 17:41:21 -0300

    make current user publically available

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,27 +6,25 @@ class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
     before_action :authorize_request
 
-    private
-
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
     end
 
-      def not_found
-        render json: { error: 'not_found' }
+    def not_found
+      render json: { error: 'not_found' }
+    end
+  
+    def authorize_request
+      header = request.headers['Authorization']
+      header = header.split(' ').last if header
+      begin
+        @decoded = JsonWebToken.decode(header)
+        @current_user = User.find(@decoded[:user_id])
+      rescue ActiveRecord::RecordNotFound => e
+        render json: { errors: e.message }, status: :unauthorized
+      rescue JWT::DecodeError => e
+        render json: { errors: e.message }, status: :unauthorized
       end
-    
-      def authorize_request
-        header = request.headers['Authorization']
-        header = header.split(' ').last if header
-        begin
-          @decoded = JsonWebToken.decode(header)
-          @current_user = User.find(@decoded[:user_id])
-        rescue ActiveRecord::RecordNotFound => e
-          render json: { errors: e.message }, status: :unauthorized
-        rescue JWT::DecodeError => e
-          render json: { errors: e.message }, status: :unauthorized
-        end
-      end
+    end
 
 end

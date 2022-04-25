674b1005120a0d892ceed4d8720f6ecc9ee4d8a9
JuliCarracedo <julicarra@hotmail.com>
2022-04-25 19:50:10 -0300

    Add routes for comics index

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   scope :api, defaults: { format: :json } do
     devise_for :users, controllers: { sessions: :sessions, registrations: :registrations },
                        path_names: { sign_in: :login }
-    resources :users, only: [:show, :update, :delete]
+    resources :users, only: [:show, :update, :delete] do
+      get "/comics", to: "comics#index"
+    end
     resources :comics, only: [:show, :create, :update, :delete] do
       get "/followers", to: "followers#list"
       post "/follow", to: "followers#follow"

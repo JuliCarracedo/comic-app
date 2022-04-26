278fd11851155c9b4336835e3b03a708033dc1af
JuliCarracedo <julicarra@hotmail.com>
2022-04-26 13:30:37 -0300

    Update routes and add like resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,15 @@ Rails.application.routes.draw do
   scope :api, defaults: { format: :json } do
     devise_for :users, controllers: { sessions: :sessions, registrations: :registrations },
                        path_names: { sign_in: :login }
-    resources :users, only: [:show, :update, :delete] do
+    resources :users, only: [:show, :update, :destroy] do
       get "/comics", to: "comics#index"
     end
-    resources :comics, only: [:show, :create, :update, :delete] do
+    resources :comics, only: [:show, :create, :update, :destroy] do
       get "/followers", to: "followers#list"
       post "/follow", to: "followers#follow"
       delete "/unfollow", to: "followers#unfollow"
       get "/likes", to: "likes#count"
+      resources :likes, only: [:create, :destroy]
     end
     get "your_likes", to: "likes#your_likes"
     # post 'user/upload', to: 'users#upload_profile'

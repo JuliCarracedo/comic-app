fc591d82a16a69e8905d87c050cc05ac69ab3fb3
JuliCarracedo <julicarra@hotmail.com>
2022-05-10 12:19:01 -0300

    Remove Comic Thumbnail from migration

--- db/migrate/20220316152729_create_comics.rb
+++ db/migrate/20220316152729_create_comics.rb
@@ -3,7 +3,6 @@ class CreateComics < ActiveRecord::Migration[6.1]
     create_table :comics do |t|
       t.string :title
       t.text :description
-      t.string :thumbnail_url
       t.references :user, null: false, foreign_key: true
 
       t.timestamps

63a31608f360870c836130d78bda7a8e025b9fee
JuliCarracedo <julicarra@hotmail.com>
2022-05-10 17:10:43 -0300

    Remove commented code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,11 @@
 class UsersController < ApplicationController
     protect_from_forgery with: :null_session
 
-    # def update
-    #   if current_user.update_attributes(user_params)
-    #     render :show
-    #   else
-    #     render json: { errors: current_user.errors }, status: :unprocessable_entity
-    #   end
-    # end
     def update
-      if current_user.update(user_params)
-        current_user.profile.attach(user_params[:avatar]) if user_params[:avatar]
-        render json: {message: "Successfully updated", user: {...current_user, profile_url: cloudinary_url(user.avatar.key, width: 100, height: 100)}}, status: 200
+      
+      if current_user.update()
+        current_user.profile.attach(params[:avatar]) if params[:avatar]
+        render json: {message: "Successfully updated", user: {profile_url: cloudinary_url(user.profile.key, width: 100, height: 100)}}, status: 200
       else
         render json: {error: current_user.errors}, status: 422
       end
@@ -27,16 +21,10 @@ class UsersController < ApplicationController
       if params[:id]
         user = User.find(params[:id])
         if user
-          render json: {user: {...user, profile_url: cloudinary_url(user.avatar.key, width: 100, height: 100)}}, status: 200
+          render json: {user: {profile_url: cloudinary_url(user.profile.key, width: 100, height: 100)}}, status: 200
         else
           render json: {error: {user:["not found"]}}, status: 422
         end
       end
-    end
-
-    private
-
-    def user_params
-      params.require(:user).permit(:username)
     end
 end

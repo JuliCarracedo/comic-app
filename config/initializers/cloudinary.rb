d84687d1fec2181c0e3ea34966104874f085179a
JuliCarracedo <julicarra@hotmail.com>
2022-05-10 11:51:39 -0300

    Add cloudinary setup as initializer

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary.config do |config|
+    config.cloud_name = 'comic-app'
+    config.api_key = '735647516826434'
+    config.api_secret = 'Wb9b3YFxaMeRVJZ7YBAlN6H4y9U'
+    config.secure = true
+    config.cdn_subdomain = true
+  end

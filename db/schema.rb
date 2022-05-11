4d196ace57bcf3f0f7089eff696316f32b9f1d0b
JuliCarracedo <julicarra@hotmail.com>
2022-05-11 14:45:19 -0300

    Migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_24_223304) do
+ActiveRecord::Schema.define(version: 2022_03_24_222640) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +27,6 @@ ActiveRecord::Schema.define(version: 2022_03_24_223304) do
   create_table "comics", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.string "thumbnail_url"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -52,13 +51,6 @@ ActiveRecord::Schema.define(version: 2022_03_24_223304) do
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
-  create_table "pages", force: :cascade do |t|
-    t.integer "number"
-    t.string "page_url"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -66,7 +58,6 @@ ActiveRecord::Schema.define(version: 2022_03_24_223304) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "username", default: "", null: false
-    t.string "profile_url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
